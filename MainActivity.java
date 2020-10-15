cc55d20ad908261cef6ea70d1410357802dd4588
anggimaulidita <62340370+anggimaulidita@users.noreply.github.com>
2020-10-15 21:33:13 +0700

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-int quantity =2;
+int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +29,16 @@ int quantity =2;
     }
 
     public void submitOrder(View view) {
-        displayPrice(quantity * 5000);
+            int price = quantity * 5000;
+        String priceMessage = "Total pembelian Rp " + price;
+        priceMessage = priceMessage + "\nTerimakasih Kakak";
+        displayMassage(priceMessage);
     }
 
+        private void displayMassage(String message) {
+            TextView priceTextView = findViewById(R.id.price_text_view);
+            priceTextView.setText(message);
+        }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view)
         quantityTextView.setText("" + number);
